802a29e4b24b5fbe5408ea066abe282da60d89e7
tejasbhaskar <157837710+tejasbhaskar@users.noreply.github.com>
2024-05-06 01:18:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function data(){
+    var a= document.getElementById("n1").value;
+    var b=document.getElementById("n2").value;
+    var c=document.getElementById("n3").value;
+    var d=document.getElementById("n4").value;
+}
+var a=4;
+var b=3;
+console.log(a+b);
+console.log("5"+3)
+console.log("5"-3)
+console.log("5"*3)
+console.log(NaN)
+console.log(+12)
+console.log("12"/2)
